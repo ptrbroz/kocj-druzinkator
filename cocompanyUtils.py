7342a472cc7a742c8454373fb1ee997c5ba4ef54
brozpet4 <brozpet4@fel.cvut.cz>
2023-09-25 23:13:59 +0200

    Excel to python history matrix conversion function done

--- cocompanyUtils.py
+++ cocompanyUtils.py
@@ -1,9 +1,12 @@
 from openpyxl import load_workbook
+import numpy as np
+import logging as log
 
 
 def vojtaToHistoryMatrix(filename):
     """
-    Convert Vojta's excel table into a history matrix.
+    Convert Vojta's excel table into a history matrix and list of names.
+    Logs warnings if something seems off about the data.
 
     Params:
     ---
@@ -12,8 +15,12 @@ def vojtaToHistoryMatrix(filename):
 
     Returns:
     ---
-        todo
-
+    personNameList : list
+        List of strings in format "Name Surname"
+    historyMatrix : np.array
+        Array size [# of people, # of years]. Elements of array are zero if person was 
+        not in a recognised company that year (or if their company is unknown).
+        If an element is nonzero, person was in company whose index matches the element.
     """
     excel = load_workbook(filename=f"./{filename}")
     sheet = excel["STATISTIKA"]
@@ -32,6 +39,7 @@ def vojtaToHistoryMatrix(filename):
     ROW_PERSON_FIRST = 11
     ROW_PERSON_LAST = len(cols[0]) - 1
     YEARS_TOTAL = COL_YEAR_LAST - COL_YEAR_FIRST + 1
+    PERSONS_TOTAL = ROW_PERSON_LAST - ROW_PERSON_FIRST + 1
     
 
     #prepare translation from companies to ints. Intended mapping: valid companies -> {1,2,3,4}, kitchen/org/visits/not present -> 0
@@ -45,13 +53,43 @@ def vojtaToHistoryMatrix(filename):
                     count = len(d.keys()) + 1
                     d[cell] = count
 
-    for d in compDicts:
-        print(d)
+    historyMatrix = np.zeros((PERSONS_TOTAL, YEARS_TOTAL))
+
+    for year in range(YEARS_TOTAL):
+        d = compDicts[year]
+        yearCol = cols[COL_YEAR_FIRST + year]
+        for person in range(PERSONS_TOTAL):
+            result = d.get(yearCol[ROW_PERSON_FIRST + person])
+            if result: 
+                historyMatrix[person, year] = result
+
+    personNameList = []
+    for i in range(PERSONS_TOTAL):
+        joinedName = cols[COL_NAME][ROW_PERSON_FIRST+i] + " " + cols[COL_SURNAME][ROW_PERSON_FIRST+i]
+        personNameList.append(joinedName)
+        #ain't nobody got time for maiden names, sorry
+
+    #check for anomalies and log them
+    incompleteYears = []
+    for i, d in enumerate(compDicts):
+        l = len(d)       
+        if l < 4:
+            incompleteYears.append(i)
+        elif l > 4:
+            y = rows[0][COL_YEAR_FIRST+i]
+            log.warning(f"Year {y} contains too many ({l}) companies! Dictionary follows: \n {d}")
+    log.info(f'''Incomplete data (fewer than 4 companies found) for following years:
+              {[rows[0][COL_YEAR_FIRST+i] for i in incompleteYears]}''')
+
+    return personNameList, historyMatrix
 
 
 
 if __name__ == "__main__":
-    vojtaToHistoryMatrix("tabory_ucastnici.xlsx")
+    log.basicConfig()
+    log.getLogger().setLevel(log.DEBUG)
+    nameList, historyMatrix = vojtaToHistoryMatrix("tabory_ucastnici.xlsx")
+
 
 
 
